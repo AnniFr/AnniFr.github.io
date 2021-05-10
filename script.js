a55d1d1ee1dbd024848bd35c6252ec094085f74b
Anni Frith <81589781+AnniFr@users.noreply.github.com>
2021-05-10 13:52:41 +0100

    adding find your hat project

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ const openPopup = () => {
             popuptext.innerHTML = 'The aim was to create a responsive site for a fictional club that would adapt to different screen sizes.';
             document.getElementById('sitelink').href = './Ramblers-site/index.html';
             break;
+        case 'hat':
+            popuptext.innerHTML = 'This project involved creating a simple node.js game that works in the terminal. I added extra features such as hard mode and some colour customisation.';
+            document.getElementById('go').innerHTML = 'GitHub Repository';
+            document.getElementById('sitelink').href = 'https://github.com/AnniFr/Find-your-hat';
+            break;
+        default:
+            popuptext.innerHTML = 'Add project description.';
+
+        
     }
 
     const overlay = document.getElementById('overlay');
@@ -26,6 +35,7 @@ const openPopup = () => {
 const closePopup = () => {
     document.getElementById('overlay').style.display = '';
     document.getElementById('popbox').style.display = '';
+    document.getElementById('go').innerHTML = 'Go to site';
 }
 
 //adds event listener to each item with class project
